56f9b4cbc8cbce948a26202a34ab9c2b928f4d15
Farris <farris.ismati@gmail.com>
2017-12-20 16:38:12 -0500

    style updates

--- src/Components/Main/Header/Header.js
+++ src/Components/Main/Header/Header.js
@@ -6,12 +6,12 @@ import                            './Header.css'
 class Header extends Component{
   render() {
     return(
-        <div className='header-holder'>
+        <div className='flex flex-space-around header-holder'>
           <Link to='/' style={{ textDecoration: 'none' }}>
             <h1>You Suck at <span>Art</span></h1>
           </Link>
           <Link to='/about' style={{ textDecoration: 'none' }}>
-            <h1 className='about-page'> About </h1>
+            <h1> About </h1>
           </Link>
         </div>
     )
