1584dc82333c25add3b234e27d0ffaf3dcc03e21
Simplejack17 <jack.tyburski1@gmail.com>
2017-12-20 11:34:49 -0500

    Added about page and added link to header

--- src/Components/Main/Header/Header.js
+++ src/Components/Main/Header/Header.js
@@ -1,6 +1,9 @@
 import React, { Component } from  'react'
 import { Link } from              'react-router-dom'
 
+import About from                            '../../SubComponents/About/About'
+
+
 import                            './Header.css'
 
 class Header extends Component{
@@ -10,6 +13,9 @@ class Header extends Component{
         <Link to='/' style={{ textDecoration: 'none' }}>
             <h1>You Suck at <span>Art</span></h1>
         </Link>
+          <Link to='/about'>
+          <h1 className='aboutPage'> About </h1>
+          </Link>
       </div>
     )
    }
